ffdf4ee3a70fc02315428e16b7c54a796de56f3a
MarcoLGP <marcolucagp@gmail.com>
2024-04-28 22:45:59 -0300

    final_commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  env: 'dev',
+  apiUrl: 'sua_api_url_dev',
+  urlBase: 'sua_url_base_dev',
+  cryptoKey: 'sua_chave_criptografia_dev',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  env: 'prod',
+  apiUrl: 'sua_api_url_prod',
+  urlBase: 'sua_url_base_prod',
+  cryptoKey: 'sua_chave_criptografia_prod',
+};
